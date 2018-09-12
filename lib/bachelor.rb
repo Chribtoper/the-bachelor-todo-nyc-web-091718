ad4816f5ac8fda304c1de49512967661960bf4a1
Chribtoper <xcstolfax@gmail.com>
2018-09-12 18:11:45 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -7,7 +7,11 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestant|
+    contestant.each do |data|
+      puts data
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
