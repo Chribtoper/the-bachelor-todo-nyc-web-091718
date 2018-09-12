2d5e522564ee2b437d646949e56a33fa11771e65
Chribtoper <xcstolfax@gmail.com>
2018-09-12 19:11:00 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -41,7 +41,7 @@ end
 def get_average_age_for_season(data, season)
 r = 0
   ages = data[season].collect do |contestant|
-    contestant["age"].to_i
+    contestant["age"].to_i.round
   end
   ages.each do |x|
     r += x
