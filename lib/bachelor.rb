6f78499a5c79b79ea20c8d776d20a0edc467aae3
Chribtoper <xcstolfax@gmail.com>
2018-09-12 18:13:47 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -9,7 +9,9 @@ end
 def get_contestant_name(data, occupation)
   data.each do |season, contestant|
     contestant.each do |data|
-      puts data["name"]
+      if data["occupation"] == occupation
+        return data["name"]
+      end
     end
   end
 end
