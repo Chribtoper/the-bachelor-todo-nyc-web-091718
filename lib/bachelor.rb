d4dcaf45de46f1019430f6bf40d042bf252520ba
Chribtoper <xcstolfax@gmail.com>
2018-09-12 17:58:19 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,8 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestant, contestant_hash|
+    puts contestant
+    puts contestant_hash
+  end
 end
 
 def get_contestant_name(data, occupation)
