eb69981020652f9403b996e7343aefcff41ecef3
Chribtoper <xcstolfax@gmail.com>
2018-09-12 18:19:03 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -17,7 +17,14 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  i = 0
+  data.each do |season, contestant|
+    contestant.each do |data|
+      if data["hometown"] == hometown
+        i += 1
+      end
+    end
+  end
 end
 
 def get_occupation(data, hometown)
